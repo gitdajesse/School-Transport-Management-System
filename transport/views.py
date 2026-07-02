b66b9270ad8ff036721a8cecb056cae0de8772f3
lavishdotcom <jessemasha05@gmail.com>
2026-07-02 17:20:09 +0300

    Implement user registration and login

--- transport/views.py
+++ transport/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import User, Parent, Bus, Student, Assistant, Attendance, Notification, RouteAssignment
 
 # Create your views here.
+def index(request):
+    return render (request, 'transport/index.html')
+
+def register(request):
+    return render (request, 'transport/register.html')
+
+def login(request):
+    return render (request, 'transport/login.html')
+
+def logout(request):
+    pass
+
+
+
+
