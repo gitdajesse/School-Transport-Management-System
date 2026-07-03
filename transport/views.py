4a3edaa07c8d6a5e48b6b43db02db7d305b23f73
lavishdotcom <jessemasha05@gmail.com>
2026-07-03 17:16:43 +0300

    Updated user registartion and login

--- transport/views.py
+++ transport/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
 from .models import User, Parent, Bus, Student, Assistant, Attendance, Notification, RouteAssignment
 
 # Create your views here.
 def index(request):
+    """ Home page - redirects to appropraite dashboard if logged in """
+
     return render (request, 'transport/index.html')
 
 def register(request):
@@ -15,5 +21,13 @@ def logout(request):
     pass
 
 
+def parent_dashboard(request):
+    return render (request, 'transport/parent_dashboard.html')
 
 
+def assistant_dashboard(request):
+    return render (request, 'transport/assistant_dashboard.html')
+
+
+def admin_dashboard(request):
+    return render (request, 'transport/admin_dashboard.html')
